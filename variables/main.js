2f9cc9e948f2c40b60a61da5c2dfa4217c4e7383
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-07-10 16:36:17 +0300

    задача 3

--- variables/main.js
+++ variables/main.js
@@ -1,5 +1,7 @@
-let name = 'Унылый Странник'
-let message = `Привет, мир! Меня зовут ${name}.`
-console.log(message);
+function excesize(x, y, z) {
+
+    console.log(x * y + 5 * z + x - 1);
+};
+excesize (2, 22, 0)
 
 
