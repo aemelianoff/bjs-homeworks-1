ea9008695753e9ef6c03b5802bbdd6dac0213c68
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-07-10 15:58:58 +0300

    задача 2

--- variables/main.js
+++ variables/main.js
@@ -1,9 +1,5 @@
-let algebra = 5;
-let geography = 5;
-let physics = 2;
-let summ;
-summ = (algebra + geography + physics)/3;
-console.log(summ);
-
+let name = 'Унылый Странник'
+let message = `Привет, мир! Меня зовут ${name}.`
+console.log(message);
 
 
