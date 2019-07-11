c9259bd1ead12beb67bc620688301e20d623b216
Емельянов Александр Геннадьевич <A.Emelianov@gazprom-media.com>
2019-07-11 10:27:54 +0300

    все 3 задачи

--- variables/main.js
+++ variables/main.js
@@ -1,3 +1,17 @@
+//////////задача 1//////////
+let algebra = 5;
+let geography = 5;
+let physics = 2;
+let summ;
+summ = (algebra + geography + physics)/3;
+console.log(summ);
+
+//////////задача 2//////////
+let name = 'Унылый Странник'
+let message = `Привет, мир! Меня зовут ${name}.`
+console.log(message);
+
+//////////задача 3//////////
 function excesize(x, y, z) {
 
     console.log(x * y + 5 * z + x - 1);
