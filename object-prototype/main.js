26f9e98ae4feae5498e008f4541e5c4eb0e1c00e
aemelianoff <51917400+aemelianoff@users.noreply.github.com>
2019-07-26 10:41:04 +0300

    исправления и порядок задач

--- object-prototype/main.js
+++ object-prototype/main.js
@@ -1,14 +1,4 @@
-function initCheckBirthday() {
-    const birthday = document.getElementById('birthday').value;
-
-    const result = checkBirthday(birthday) ? "Да" : "Нет";
-
-    document.getElementById('disclaimer').innerHTML = result;   
-}
-
-function checkBirthday(birthday) {
-    // код для задачи №1 писать здесь
-}
+//Задача 1
 
 function initPrintAnimalSound() {
     const animal = {
@@ -21,16 +11,17 @@ function initPrintAnimalSound() {
 }
 
 function getAnimalSound(animal) {
-    // код для задачи №2 писать здесь
+    // код для задачи №1 писать здесь
     let sound = animal.sound;
-    this.animal = animal;
     if (animal === undefined) {
-        return null;
+      return null;
     } 
     else if (animal) {
-    return sound;
+      return sound;
     }
 }
+
+//Задача 2
 
 function initCalculateStatement() {
     for (let idx = 0; idx < 3; idx++) {
@@ -43,11 +34,25 @@ function initCalculateStatement() {
 }
 
 function getAverageMark(marks) {
+    // код для задачи №2 писать здесь
+  let sum = 0;
+  for (let i = 0; i < marks.length; i++) {
+    sum += parseFloat(marks[i]);
+  }
+  let roundedAverage = Math.round(sum / marks.length);
+  return roundedAverage;
+}
+
+//Задача 3
+
+function initCheckBirthday() {
+    const birthday = document.getElementById('birthday').value;
+
+    const result = checkBirthday(birthday) ? "Да" : "Нет";
+
+    document.getElementById('disclaimer').innerHTML = result;   
+}
+
+function checkBirthday(birthday) {
     // код для задачи №3 писать здесь
-    let sum = 0;
-	for (let i = 0; i < marks.length; i++) {
-		sum += Number(marks[i]);
-	}
-    let roundedAverage = Math.round(sum / marks.length);
-    return roundedAverage;
 }
